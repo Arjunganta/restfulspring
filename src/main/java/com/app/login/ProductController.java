1831ec35749af1508294635af82396e2fec4e43e
Arjunganta@github.com <arjunganta007@gmail.com>
2021-09-30 17:36:22 +0000

    Added new files

---
+++ src/main/java/com/app/login/ProductController.java
@@ -0,0 +1,30 @@
+
+package com.app.login;
+import java.util.List;  
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;  
+import org.springframework.web.bind.annotation.GetMapping;  
+import org.springframework.web.bind.annotation.RestController;  
+@RestController  
+public class ProductController   
+{  
+@Autowired  
+private ProductService productService;  
+
+@RequestMapping("/")
+String getSomething(){
+  return "Running on Heroku";
+}
+
+
+//mapping the getProduct() method to /product  
+@GetMapping(value = "/product")  
+public List<Product> getProduct()   
+{  
+//finds all the products  
+List<Product> products = productService.findAll();  
+//returns the product list  
+return products;  
+}  
+}  
