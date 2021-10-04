94277284b1886616184fdfba8ca6bd2d6ab0b0c0
Arjunganta@github.com <arjunganta007@gmail.com>
2021-10-04 20:35:07 +0000

    Added @CorsOrigin

--- src/main/java/com/app/login/ProductController.java
+++ src/main/java/com/app/login/ProductController.java
@@ -5,7 +5,8 @@ import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;  
 import org.springframework.web.bind.annotation.GetMapping;  
-import org.springframework.web.bind.annotation.RestController;  
+import org.springframework.web.bind.annotation.RestController;
+@CrossOrigin(origins="http://localhost:4200")  
 @RestController  
 public class ProductController   
 {  
